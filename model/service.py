e3a64c08cfbd1529daa5b15480acd2b0d9d95752
Joaquim Perez <jquim1@hotmail.com>
2021-04-02 12:34:16 +0100

    tf 1.5 adjustments

--- model/service.py
+++ model/service.py
@@ -28,18 +28,18 @@ class ModelService:
         min_charging_period_energy_spent = ModelService.CHARGING_PERIOD_PEAK_AVG - ModelService.CHARGING_PERIOD_PEAK_STDDEV
         max_charging_period_energy_spent = ModelService.CHARGING_PERIOD_PEAK_AVG + ModelService.CHARGING_PERIOD_PEAK_STDDEV
 
-        tf_random = tf.random.uniform(
+        tf_random = tf.random_uniform(
                 shape=shape,
                 minval=min_charging_period_energy_spent,
                 maxval=max_charging_period_energy_spent,
-                dtype=tf.dtypes.float32,
+                dtype=tf.float32,
                 seed=None,
                 name=None
         )
         tf_var = tf.Variable( tf_random )
 
-        tf_init = tf.compat.v1.global_variables_initializer( )
-        tf_session = tf.compat.v1.Session( )
+        tf_init = tf.global_variables_initializer( )
+        tf_session = tf.Session( )
         tf_session.run( tf_init )
 
         tf_return = tf_session.run( tf_var )
